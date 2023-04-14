5b88eee0c2868107ce71b11b0d6d1cc3ae0c90a7
Manas <pk@manas.com>
2023-04-14 18:26:55 +0530

    comment line added

--- app/src/main/java/com/example/bmiexample/MainActivity.java
+++ app/src/main/java/com/example/bmiexample/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.bmiexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,17 +11,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // set the layout of the activity
         setContentView(R.layout.activity_main);
 
+        // initialize UI elements
         EditText weight,feet,inch;
         Button calculate;
         TextView result;
         LinearLayout liMain;
 
+        // find views by their IDs
         weight = findViewById(R.id.weight);
         feet = findViewById(R.id.feet);
         inch = findViewById(R.id.inch);
@@ -30,21 +31,28 @@ public class MainActivity extends AppCompatActivity {
         result = findViewById(R.id.result);
         liMain = findViewById(R.id.liMain);
 
+        // set click listener for the calculate button
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // get user input values and convert them to appropriate data types
                 int wt = Integer.parseInt(weight.getText().toString());
                 int ft = Integer.parseInt(feet.getText().toString());
                 int in = Integer.parseInt(inch.getText().toString());
 
+                // convert height values to total inches
                 int totalIn = ft*12 + in;
 
+                // convert total inches to total centimeters
                 double totalCm = totalIn*2.53;
 
+                // convert total centimeters to total meters
                 double totalM = totalCm / 100;
 
+                // calculate BMI
                 double bmi = wt/Math.pow(totalM,2);
 
+                // set the result and background color based on BMI value
                 if (bmi > 25){
                     result.setText("You are OverWeight");
                     liMain.setBackgroundColor(getResources().getColor(R.color.overwt));
